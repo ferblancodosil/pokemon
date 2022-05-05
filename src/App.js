98a6112242a2eb341770e9847279b37f691ddbc9
blain <blain@argalladas.eu>
2022-05-05 11:39:46 +0200

    Created table component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import Table from "./components/Table";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const [columns] = useState(['col1', 'col2', 'col3'])
+ const [items] = useState([{col1: 'cosa1 as a das dsad ', col2: 'csa1', col3: 'srasda1'}, {col1: 'cosa2', col2: 'csa2', col3: 'srasda2'}])
+ return (
+  <div className="App">
+   <Table columns={columns} items={items}></Table>
+  </div>
+ );
 }
 
 export default App;
---
+++ src/components/Table/index.jsx
@@ -0,0 +1,43 @@
+import VirtualScroll from "react-dynamic-virtual-scroll"
+import './index.scss'
+import PropTypes from 'prop-types'
+import {useCallback} from "react";
+
+function Table({ height = '100vh', minItemHeight = 40, items, buffer = 10, columns = [] } = {}) {
+ const rowItem = useCallback((rowIndex) => {
+  return (
+   <div className="tr">
+    {columns.map((column, index) => <div key={index} className="td"><div className="caption">{column}</div> <div className="data">{items[rowIndex][column]}</div></div>)}
+   </div>
+  )
+ }, [items, columns]);
+
+ return (
+    <>
+     <div className="mytable">
+      <div className="thead">
+       {columns.map((column, index) => <div key={index} className="th">{column}</div>)}
+      </div>
+        <VirtualScroll
+         className="tbody"
+         style={{ height, overflowY: 'auto' }}
+         minItemHeight={minItemHeight}
+         totalLength={items.length}
+         renderItem={rowItem}
+           buffer={buffer}
+        />
+      </div>
+    </>
+  )
+}
+
+
+Table.propTypes = {
+  height: PropTypes.string,
+  minItemHeight: PropTypes.string,
+  items: PropTypes.array.isRequired,
+  buffer: PropTypes.number,
+  columns: PropTypes.array.isRequired
+};
+
+export default Table
